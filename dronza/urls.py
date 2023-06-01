862d0f61f62e02d8ab82c644f1d28b3fdcd186a7
Sohaib_LangriaL <52604138+LaaL-Khan-LangriaL@users.noreply.github.com>
2023-06-01 21:46:27 +0500

    redirect fro non www added

--- dronza/urls.py
+++ dronza/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('django_admin', admin.site.urls, name="django_admin"),
-    path('', RedirectView.as_view(url='http://dronza.org{path}', permanent=True)),
     path('', include('dronzaPanel.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
