559a18862c2271d21adc2c90b95de0f5fed0369f
Sohaib_LangriaL <52604138+LaaL-Khan-LangriaL@users.noreply.github.com>
2023-06-01 21:27:36 +0500

    redirect changes for non www

--- dronza/urls.py
+++ dronza/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='https://dronza.org', permanent=True)),
     path('django_admin', admin.site.urls, name="django_admin"),
     path('', include('dronzaPanel.urls')),
     path('', include('home.urls')),
