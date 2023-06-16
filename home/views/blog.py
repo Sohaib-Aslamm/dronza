11ce31eb2ee7b5c51ec84bd2270d63464cc3f9a2
Sohaib_LangriaL <52604138+LaaL-Khan-LangriaL@users.noreply.github.com>
2023-06-16 11:18:44 +0500

    blog page canonical changed

--- home/views/blog.py
+++ home/views/blog.py
@@ -71,7 +71,7 @@ def postDetail(request, slug):
     SEOTAGS = [{'title': rdPost.first().title,
                 'description': rdPost.first().title,
                 'tags': rdPost.first().title,
-                'canonical_link': f'https://dronza.org/{rdPost.first().title}'}]
+                'canonical_link': f'https://dronza.org/{rdPost.first().slug}'}]
 
     context = {
         'rdPost': rdPost,
