7977c636c3e1fc6b8b42296d592020cfef905619
Sohaib_LangriaL <52604138+LaaL-Khan-LangriaL@users.noreply.github.com>
2023-06-16 14:19:13 +0500

    blog page added seperate for search blog

--- home/views/blog.py
+++ home/views/blog.py
@@ -33,32 +33,41 @@ def blog(request):
 
 
 def search_blog(request):
-    if request.method == 'POST':
-        search_keyword = request.POST.get('search_keyword')
-        blog_data = userBlog.objects.filter(Q(title__icontains=search_keyword) | Q(heading__icontains=search_keyword) | Q(description__icontains=search_keyword)).values('sNo', 'title', 'heading', 'Icon', 'created_at').order_by('-sNo')
-        paginator = Paginator(blog_data, 9)
-        page_number = request.GET.get('page')
-        blog_data_final = paginator.get_page(page_number)
-        total_pages = blog_data_final.paginator.num_pages
+    search_keyword = request.GET.get('search-keyword') or request.POST.get('search_keyword')
+    blog_data = userBlog.objects.all()  # Get all blog data initially
 
-        recent_posts = userBlog.objects.order_by('-sNo')[:2]
-        popular_posts = userBlog.objects.values('sNo', 'title', 'heading', 'slug', 'Icon', 'created_at').order_by('-sNo')[10:16]
-        featured_products = Products.objects.filter(featured='Featured').order_by('-id')[:4]
+    if search_keyword:
+        blog_data = blog_data.filter(
+            Q(title__icontains=search_keyword) |
+            Q(heading__icontains=search_keyword) |
+            Q(description__icontains=search_keyword)
+        ).values('sNo', 'title', 'heading', 'slug', 'Icon', 'created_at').order_by('-sNo')
 
-        SMDT = SocialMedia.objects.all()
-        SEOTAGS = seoTags.objects.filter(page='search_blog')
+    paginator = Paginator(blog_data, 9)
+    page_number = request.GET.get('page')
+    blog_data_final = paginator.get_page(page_number)
+    total_pages = blog_data_final.paginator.num_pages
 
-        context = {
-            'BLOGDATA': blog_data_final,
-            'lastPage': total_pages,
-            'pageList': range(1, total_pages + 1),
-            'RCPST': recent_posts,
-            'Blog_RCPST': popular_posts,
-            'Top_Products': featured_products,
-            'SMDT': SMDT,
-            'SEOTAGS': SEOTAGS
-        }
-        return render(request, 'Blog.html', context)
+    recent_posts = userBlog.objects.order_by('-sNo')[:2]
+    popular_posts = userBlog.objects.values('sNo', 'title', 'heading', 'slug', 'Icon', 'created_at').order_by('-sNo')[10:16]
+    featured_products = Products.objects.filter(featured='Featured').order_by('-id')[:4]
+
+    SMDT = SocialMedia.objects.all()
+    SEOTAGS = seoTags.objects.filter(page='search_blog')
+
+    context = {
+        'SEARCHDATA': blog_data_final,
+        'lastPage': total_pages,
+        'pageList': range(1, total_pages + 1),
+        'RCPST': recent_posts,
+        'Blog_RCPST': popular_posts,
+        'Top_Products': featured_products,
+        'SMDT': SMDT,
+        'SEOTAGS': SEOTAGS,
+        'search_keyword': search_keyword,  # Include search_keyword in the context
+    }
+    return render(request, 'search_blog.html', context)
+
 
 
 def postDetail(request, slug):
