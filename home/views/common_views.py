c2cafa509573203fb24f79cf8d6124b3ffa82802
Sohaib_LangriaL <52604138+LaaL-Khan-LangriaL@users.noreply.github.com>
2023-06-20 01:18:32 +0500

    all pages have dynamic slider and title

--- home/views/common_views.py
+++ home/views/common_views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from home.models import userBlog
-from dronzaPanel.models import SocialMedia, seoTags
+from dronzaPanel.models import SocialMedia, seoTags, MainSlider
 
 
 def thankyou(request):
@@ -15,9 +15,11 @@ def coming_soon(request):
     RCPST = userBlog.objects.order_by('-sNo')[:2]
     SMDT = SocialMedia.objects.all()
     SEOTAGS = seoTags.objects.filter(page='coming_soon')
+    SLIDER = MainSlider.objects.filter(page='coming_soon_page')
     context = {
         'RCPST': RCPST,
         'SMDT': SMDT,
-        'SEOTAGS': SEOTAGS
+        'SEOTAGS': SEOTAGS,
+        'SLIDER': SLIDER,
     }
     return render(request, 'error404.html', context)
