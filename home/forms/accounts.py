c2262ee49a1fc0bfb6bc3bc52de5e73504c876e8
Sohaib_LangriaL <52604138+LaaL-Khan-LangriaL@users.noreply.github.com>
2024-01-24 13:36:07 +0500

    registration form required added for first and last name and email

--- home/forms/accounts.py
+++ home/forms/accounts.py
@@ -10,10 +10,10 @@ class UserForm(UserCreationForm):
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'username'}),
-            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'first name'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'last name'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'first name', 'required': 'required'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'last name', 'required': 'required'}),
             'email': forms.EmailInput(attrs={'class': 'form-control',
-                                             'placeholder': 'enter valid e-mail'}),
+                                             'placeholder': 'enter valid e-mail', 'required': 'required'}),
             'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'choose password'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'confirm password'}),
         }
