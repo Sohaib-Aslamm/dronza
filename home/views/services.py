79cb8e369c6ae1a14c9493bd1f6fdb6707c7c676
Sohaib_LangriaL <52604138+LaaL-Khan-LangriaL@users.noreply.github.com>
2024-01-25 12:34:32 +0500

    services structure changes for regular services

--- home/views/services.py
+++ home/views/services.py
@@ -8,10 +8,10 @@ def services(request):
 
     context = {
         'RegularServices': ServicesTypes.objects.filter(type=ServicesType.REGULAR_SERVICE),
-        'PRCDT': Pricing.objects.all(),
+        'pricing_table': Pricing.objects.all(),
         'RCPST': userBlog.objects.order_by('-sNo')[:2],
         'SMDT': SocialMedia.objects.all(),
-        'MAINSERVICES': ServicesTypes.objects.filter(type=ServicesType.MAIN_SERVICE),
+        'MainServices': ServicesTypes.objects.filter(type=ServicesType.MAIN_SERVICE),
         'SEOTAGS': seoTags.objects.filter(page='services_page'),
         'SLIDER': MainSlider.objects.filter(page='services_page')
     }
